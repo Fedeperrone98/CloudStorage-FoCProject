4713ab63b08f612dbc94713e53af488c75405841
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2022-05-08 17:34:35 +0200

    add initial code

---
+++ server.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <openssl/bio.h>
+#include <openssl/buffer.h>
+#include <openssl/des.h>
+#include <openssl/evp.h>
+#include <openssl/pem.h>
+#include <openssl/rsa.h>
+#include <termios.h>
+#include "include/server.h"
+
+using namespace std;
+
+int main(int argc, char* const argv[]) {
+
+    return 0;
+}
