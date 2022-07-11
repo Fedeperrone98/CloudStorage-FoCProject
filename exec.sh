0b225b96322355d8b82a996d34f5d00278b5d517
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2022-07-11 16:19:03 +0200

    update exec.sh

--- exec.sh
+++ exec.sh
@@ -6,7 +6,7 @@
 # 2. ESECUZIONE
 
 # 2.1 esecuzioe del server sulla porta 4242
-  gnome-terminal -x sh -c "./ds 4242; exec bash"
+  gnome-terminal -x sh -c "./server 4242; exec bash"
 
 # 2.2 esecuzione del client sulla porta
-  gnome-terminal -x sh -c "./peer 5001; exec bash"
+  gnome-terminal -x sh -c "./client 5001; exec bash"
